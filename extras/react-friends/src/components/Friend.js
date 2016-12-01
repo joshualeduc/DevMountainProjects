2142aba1759d6ddc356a1952d4e0eb80957e587f
Joshua LeDuc <joshualeduc@gmail.com>
2016-12-01 13:45:47 -0700

    finished react-friends

---
+++ extras/react-friends/src/components/Friend.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Friend extends React.Component{
+
+  render(){
+    return(
+      <li className="friend">
+        <img className="profile-pic" src="{this.props.pictureUrl}" />
+        <h3>{this.props.name}</h3>
+        <div className="location">
+          Location: {this.props.currentLocation.city}, {this.props.currentLocation.state}, {this.props.currentLocation.country}
+        </div>
+        <div className="status">
+          Status: {this.props.status}
+        </div>
+        <div className="num-friends">
+          {this.props.friendCount}
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Friend;
